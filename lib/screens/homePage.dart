a154d32080f2e0c1d83276c70596a0fedafa54ad
Imran Hashmi <112310959+Imran6500@users.noreply.github.com>
2024-02-02 16:59:04 +0530

    bugs fixed

--- lib/screens/homePage.dart
+++ lib/screens/homePage.dart
@@ -1,5 +1,5 @@
+// ignore: file_names
 import 'package:cryptoapp/Controllers/coins_controllers.dart';
-import 'package:cryptoapp/screens/utills.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 
@@ -18,94 +18,118 @@ class HomePage extends StatelessWidget {
           child: Column(
             crossAxisAlignment: CrossAxisAlignment.start,
             children: [
-              Text("Crypto Market",
-                  style: textStyle(25, Colors.white, FontWeight.bold)),
-              Obx(() => controller.isLoading.value
-                  ? const Center(
-                      child: CircularProgressIndicator(),
-                    )
-                  : ListView.builder(
-                      shrinkWrap: true,
-                      physics: const NeverScrollableScrollPhysics(),
-                      itemCount: 10,
-                      itemBuilder: (context, index) {
-                        return Padding(
-                          padding: const EdgeInsets.only(bottom: 20),
-                          child: Container(
-                            width: MediaQuery.of(context).size.width,
-                            height: 60,
-                            child: Row(
-                              mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                              children: [
-                                Row(
-                                  children: [
-                                    Container(
-                                      width: 60,
-                                      height: 60,
-                                      decoration: BoxDecoration(
-                                        color: Colors.grey[700],
-                                        borderRadius: BorderRadius.circular(15),
-                                        boxShadow: [
-                                          BoxShadow(
-                                              color: Colors.grey[700]!,
-                                              offset: Offset(4, 4),
-                                              blurRadius: 5)
-                                        ],
+              const Text("Crypto Market",
+                  style: TextStyle(
+                      fontSize: 25,
+                      color: Colors.white,
+                      fontWeight: FontWeight.bold)),
+              Obx(
+                () => controller.isLoading.value
+                    ? const Center(
+                        child: CircularProgressIndicator(),
+                      )
+                    : ListView.builder(
+                        shrinkWrap: true,
+                        physics: const NeverScrollableScrollPhysics(),
+                        itemCount: controller.coinsList.length,
+                        itemBuilder: (context, index) {
+                          return Padding(
+                            padding: const EdgeInsets.only(bottom: 20),
+                            child: Container(
+                              // color: Colors.red,
+                              // width: 150,
+                              height: 65,
+                              child: Row(
+                                children: [
+                                  Row(
+                                    mainAxisAlignment: MainAxisAlignment.start,
+                                    children: [
+                                      Container(
+                                        width: 60,
+                                        height: 60,
+                                        decoration: BoxDecoration(
+                                          color: Colors.grey[700],
+                                          borderRadius:
+                                              BorderRadius.circular(15),
+                                          boxShadow: [
+                                            BoxShadow(
+                                                color: Colors.grey[700]!,
+                                                offset: const Offset(4, 4),
+                                                blurRadius: 5)
+                                          ],
+                                        ),
+                                        child: Padding(
+                                          padding: const EdgeInsets.all(10),
+                                          child: Image.network(controller
+                                              .coinsList[index].image),
+                                        ),
                                       ),
-                                      child: Padding(
-                                        padding: const EdgeInsets.all(10),
-                                        child: Image.network(
-                                            controller.coinsList[index].image),
+                                      const SizedBox(width: 20),
+                                      SizedBox(
+                                        width:
+                                            MediaQuery.of(context).size.height -
+                                                650,
+                                        child: Column(
+                                          crossAxisAlignment:
+                                              CrossAxisAlignment.start,
+                                          children: [
+                                            const SizedBox(
+                                              height: 10,
+                                            ),
+                                            Expanded(
+                                              child: Text(
+                                                  controller
+                                                      .coinsList[index].name,
+                                                  style: const TextStyle(
+                                                      fontSize: 18,
+                                                      color: Colors.white,
+                                                      fontWeight:
+                                                          FontWeight.w600)),
+                                            ),
+                                            Text(
+                                                "${controller.coinsList[index].priceChangePercentage24H.toStringAsFixed(2)} %",
+                                                style: const TextStyle(
+                                                    fontSize: 18,
+                                                    color: Colors.grey,
+                                                    fontWeight:
+                                                        FontWeight.w600))
+                                          ],
+                                        ),
                                       ),
-                                    ),
-                                    SizedBox(width: 20),
-                                    Column(
+                                    ],
+                                  ),
+                                  const SizedBox(width: 20),
+                                  Expanded(
+                                    child: Column(
                                       crossAxisAlignment:
-                                          CrossAxisAlignment.start,
+                                          CrossAxisAlignment.end,
                                       children: [
-                                        SizedBox(
+                                        const SizedBox(
                                           height: 10,
                                         ),
                                         Text(
-                                          controller.coinsList[index].name,
-                                          style: textStyle(18, Colors.white,
-                                              FontWeight.w600),
-                                        ),
+                                            "\$ ${controller.coinsList[index].currentPrice.round()}",
+                                            style: const TextStyle(
+                                                fontSize: 18,
+                                                color: Colors.white,
+                                                fontWeight: FontWeight.w600)),
                                         Text(
-                                          "${controller.coinsList[index].priceChangePercentage24H.toStringAsFixed(2)} %",
-                                          style: textStyle(
-                                              18, Colors.grey, FontWeight.w600),
-                                        )
+                                            controller.coinsList[index].symbol
+                                                .toUpperCase(),
+                                            style: const TextStyle(
+                                                fontSize: 18,
+                                                color: Colors.grey,
+                                                fontWeight: FontWeight.w600))
                                       ],
                                     ),
-                                  ],
-                                ),
-                                SizedBox(width: 20),
-                                Column(
-                                  crossAxisAlignment: CrossAxisAlignment.end,
-                                  children: [
-                                    SizedBox(
-                                      height: 10,
-                                    ),
-                                    Text(
-                                      "\$ ${controller.coinsList[index].currentPrice.round()}",
-                                      style: textStyle(
-                                          18, Colors.white, FontWeight.w600),
-                                    ),
-                                    Text(
-                                      controller.coinsList[index].symbol
-                                          .toUpperCase(),
-                                      style: textStyle(
-                                          18, Colors.grey, FontWeight.w600),
-                                    )
-                                  ],
-                                )
-                              ],
+                                  )
+                                ],
+                              ),
                             ),
-                          ),
-                        );
-                      },
-                    ))
+                          );
+                        },
+                      ),
+              )
             ],
           ),
         ),
--- lib/screens/utills.dart
+++ lib/screens/utills.dart
@@ -1,6 +1,14 @@
 import 'package:flutter/cupertino.dart';
 import 'package:google_fonts/google_fonts.dart';
 
-TextStyle textStyle(double size, Color color, FontWeight fw) {
-  return GoogleFonts.montserrat(fontSize: size, color: color, fontWeight: fw);
-}
+// TextStyle textStyle(
+//   double size,
+//   Color color,
+//   FontWeight fw,
+// ) {
+//   return GoogleFonts.montserrat(
+//     fontSize: size,
+//     color: color,
+//     fontWeight: fw,
+//   );
+// }
